1471fb287fe127df6d63e9d1a308073b5fb614ba
julian <julian.quinteror1@gmail.com>
2019-10-10 13:50:48 -0500

    Added next day api and fixed css

--- app.js
+++ app.js
@@ -1,13 +1,71 @@
-async function getWeather() {
+
+
+
+async function getweatherNow() {
 
     let data = await fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=ff26273dd44b20d6f5a756205c13e775");
     data = await data.json();
-    let items = await data;
+    let weather = await data;
+    return weather;
+      
+}
+
+async function getweatherTomorrow() {
+
+    let data = await fetch("http://api.openweathermap.org/data/2.5/forecast?q=london&appid=ff26273dd44b20d6f5a756205c13e775");
+    data = await data.json();
+    let weather = await data;
+    return weather;
+      
+}
 
 
-    console.log(items);
+
+async function render(){
     
+     
+
+      let data = await getweatherNow();
+      let tomorrowWeather= await getweatherTomorrow();
+      let icon = data.weather[0].icon;
+
+     
+      
+  
+
+
+      document.querySelector("#icon").src=`http://openweathermap.org/img/wn/${icon}@2x.png`;
+      document.querySelector("#icon2").src=`http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+      document.querySelector("#main").innerHTML=data.weather[0].main;
+      document.querySelector("#humidity").innerHTML=`Humidity: ${data.main.humidity}%`;
+      document.querySelector("#wind").innerHTML=`Wind: ${data.wind.speed} km/h`;
+      document.querySelector("#temp").innerHTML=`${Math.round(data.main.temp-273.15)} ℃`;
+      document.querySelector("#tomorrow").innerHTML=`Humidity: ${tomorrowWeather.list[4].main.temp}%`;
+
+
+
+
+
+
+      
+
+
+          
+
+}   
+
+
+
+
+function date() {
+
+    let dateDom=document.querySelector(".date");
+    dateDom.innerHTML=Date.now();
+   
+
     
 }
 
-getWeather();
+
+render();
