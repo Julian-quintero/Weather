5f95655b1ce3e191850fe728f752c79729ae0b1a
julian <julian.quinteror1@gmail.com>
2019-10-14 18:59:59 -0500

    added new icons and text input search

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
-
-
-
-async function getweatherNow() {
-
-    let data = await fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=ff26273dd44b20d6f5a756205c13e775");
-    data = await data.json();
-    let weather = await data;
-    return weather;
-      
-}
-
-async function getweatherTomorrow() {
-
-    let data = await fetch("http://api.openweathermap.org/data/2.5/forecast?q=london&appid=ff26273dd44b20d6f5a756205c13e775");
-    data = await data.json();
-    let weather = await data;
-    return weather;
-      
-}
-
-
-
-async function render(){
+var params= {
     
+    "input":{
+        "value":"Bogota"
+    }
+   
+}
+
+
+
+async function getweatherNow(city="Bogota"){
+    
+
+    let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ff26273dd44b20d6f5a756205c13e775`);
+    data = await data.json();
+    let weather = await data;
+    return weather;
+      
+}
+
+async function getweatherTomorrow(city="Bogota") {
+
+    let data = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=ff26273dd44b20d6f5a756205c13e775`);
+    data = await data.json();
+    let weather = await data;
+    return weather;
+      
+}
+
+
+
+async function render(params){
+ console.log(params.input.value);
+ 
      
 
-      let data = await getweatherNow();
-      let tomorrowWeather= await getweatherTomorrow();
+      let data = await getweatherNow(params.input.value);
+      let tomorrowWeather= await getweatherTomorrow(params.input.value);
       let icon = data.weather[0].icon;
       let iconTomorrow = tomorrowWeather.list[5].weather[0].icon;
-      console.log(tomorrowWeather);
+      console.log(data);
       
-     
-      
-  
-
-
-      document.querySelector("#icon").src=`http://openweathermap.org/img/wn/${icon}@2x.png`;
-      document.querySelector("#icon2").src=`http://openweathermap.org/img/wn/${iconTomorrow}@2x.png`;
+      document.querySelector("#icon").src=`./img/${icon}.svg`;
+      document.querySelector("#icon2").src=`./img/${iconTomorrow}.svg`;
 
       document.querySelector("#main").innerHTML=data.weather[0].main;
       document.querySelector("#descriptionTomorrow").innerHTML=tomorrowWeather.list[5].weather[0].main;
@@ -45,7 +49,7 @@ async function render(){
       document.querySelector("#humidity").innerHTML=`Humidity: ${data.main.humidity}%`;
       document.querySelector("#wind").innerHTML=`Wind: ${data.wind.speed} km/h`;
       document.querySelector("#temp").innerHTML=`${Math.round(data.main.temp-273.15)}℃`;
-      document.querySelector("#tomorrow").innerHTML=`Humidity: ${tomorrowWeather.list[4].main.temp}%`;
+      document.querySelector("#pressure").innerHTML=`Pressure: ${data.main.pressure} hPa`;
 
       document.querySelector(".loading").style.display="none";
 
@@ -68,6 +72,14 @@ function loading() {
     
 }
 
+function showElements() {
+
+    document.querySelector("#button").style.display="none";
+    document.querySelector(".form").style.display="inline";
+ }
+
+
+
 
 loading();
-render();
+render(params);
